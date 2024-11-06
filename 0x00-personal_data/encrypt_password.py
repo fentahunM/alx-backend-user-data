30ead0f28b9b0e785a7fcdfe8e50a65f45e5aafe
fentahunM <fantahun360@gmail.com>
2024-11-06 21:09:12 +0300

    task 6 check valid password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -2,6 +2,7 @@
 """Module for encrypting passwords.
 """
 
+import logging
 import bcrypt
 
 
@@ -10,3 +11,14 @@ def hash_password(password: str) -> bytes:
     """
     # Salt and hash the password using the bcrypt package
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Validates that the provided password matches the hashed password.
+    """
+    # Try to match the hashed password with the given password
+    try:
+        return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
+    # If there is an exception in the process, log the error message
+    except Exception as e:
+        logging.error("Error in password validation: {}".format(e))
+        return False
