40e781251a59ba1913cd24af1549f280d07ca23c
fentahunM <fantahun360@gmail.com>
2024-11-06 20:03:27 +0300

    task 1 log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,6 +2,7 @@
 """Module for personal data project
 """
 
+import logging
 import re
 from typing import List
 
@@ -9,6 +10,46 @@ patterns = {
     'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
     'replace': lambda x: r'\g<field>={}'.format(x),
 }
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+
+    Update the class to accept a list of strings fields constructor argument.
+    Implement the format method to filter values in incoming log records using
+    filter_datum. Values for fields in fields should be filtered.
+    DO NOT extrapolate FORMAT manually. The format method should be less than
+    5 lines long.
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """Initializes the class.
+
+        Args:
+            fields (List[str]): The fields.
+        """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Filters values in incoming log records using filter_datum.
+
+        Args:
+            record (logging.LogRecord): A logging.LogRecord instance.
+
+        Returns:
+            str: A string with all occurrences of the `self.fields` in
+            `record.message` replaced by the `self.REDACTION` string.
+        """
+        # Call the parent class's format method to get the formatted log line
+        msg = super(RedactingFormatter, self).format(record)
+        # Use the filter_datum function to perform substitution of self.fields
+        text = filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)
+        return text
 
 
 def filter_datum(
