cdc914f22c3576636418ba2d496e3faa0aa3ac93
fentahunM <fantahun360@gmail.com>
2024-11-06 21:02:45 +0300

    filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -80,10 +80,10 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     host = environ.get("PERSONAL_DATA_DB_HOST", "localhost")
     db_name = environ.get("PERSONAL_DATA_DB_NAME")
 
-    cnx = mysql.connector.connection.MySQLConnection(user=username,
-                                                     password=password,
-                                                     host=host,
-                                                     database=db_name)
+    cnx = mysql.connector.connect(user=username,
+                                  password=password,
+                                  host=host,
+                                  database=db_name)
     return cnx
 
 
