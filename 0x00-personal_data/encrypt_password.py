95b8a7bd793a4a1232bda96a4e95a61c2bff0482
fentahunM <fantahun360@gmail.com>
2024-11-06 21:12:10 +0300

    update is_valid

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -12,13 +12,11 @@ def hash_password(password: str) -> bytes:
     # Salt and hash the password using the bcrypt package
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
+
 def is_valid(hashed_password: bytes, password: str) -> bool:
-    """Validates that the provided password matches the hashed password.
-    """
-    # Try to match the hashed password with the given password
-    try:
-        return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
-    # If there is an exception in the process, log the error message
-    except Exception as e:
-        logging.error("Error in password validation: {}".format(e))
-        return False
+    """ Validates the provided password matches the hashed password """
+    valid = False
+    encoded = password.encode()
+    if bcrypt.checkpw(encoded, hashed_password):
+        valid = True
+    return valid
