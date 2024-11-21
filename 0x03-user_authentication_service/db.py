673385c325751986c2337a2993298f11fff73690
fentahunM <fantahun360@gmail.com>
2024-11-21 23:49:23 +0300

    task 1 create user

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+""" Database for ORM """
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from user import Base, User
+from typing import TypeVar
+
+
+
+
+class DB:
+    """ DB Class for Object Reational Mapping """
+
+    def __init__(self):
+        """ Constructor Method """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self):
+        """ Session Getter Method """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """ Adds user to database
+        Return: User Object
+        """
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        self._session.commit()
+
+        return user
